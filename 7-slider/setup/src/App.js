59e3d45f6c2eae31eba662b6af9024aad3fa4484
Rodrigo Novaes <rod_novaes@hotmail.com>
2020-12-08 15:35:42 +1300

    completed project 7

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -6,6 +6,25 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 5000);
+    return () => clearInterval(slider);
+  }, [index]);
+
   return (
     <section className='section'>
       <div className='title'>
@@ -17,8 +36,21 @@ function App() {
         {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
           //  more stuff coming up
+          let position = 'nextSlide';
+
+          if (personIndex === index) {
+            position = 'activeSlide';
+          }
+
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = 'lastSlide';
+          }
+
           return (
-            <article key={id}>
+            <article className={position} key={id}>
               <img src={image} alt={name} className='person-img' />
               <h4>{name}</h4>
               <p className='title'>{title}</p>
@@ -27,10 +59,10 @@ function App() {
             </article>
           );
         })}
-        <button className='prev'>
+        <button className='prev' onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className='next'>
+        <button className='next' onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
