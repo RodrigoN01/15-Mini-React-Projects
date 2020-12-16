78aca4b5af17b6a4d1aa3c22c3d158019d991c44
Rodrigo Novaes <rod_novaes@hotmail.com>
2020-12-16 17:28:31 +1300

    completed project 15

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h2 className='section-title'>about us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+        reprehenderit suscipit obcaecati aut unde a voluptatibus dolor dolore
+        laborum minima, assumenda praesentium, itaque dolorem cum libero.
+        Voluptate eveniet delectus voluptas eaque et quisquam veniam, illo
+        perferendis dignissimos maxime. Voluptatibus, expedita!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
